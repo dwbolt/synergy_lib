3401afca3b92ff1b5d30669db715be493f188918
David Bolt <David.Bolt@sfcknox.org>
2023-04-12 08:45:55 -0400

    adding REST api

--- proxy/proxyClass.js
+++ proxy/proxyClass.js
@@ -3,13 +3,40 @@ class proxyClass { // proxyClass - client - side
 /*
 
 class to work with other web servers
-this may need to be on server rather than client
 
 */
 
 constructor() {  // proxyClass - client - side
 }
 
+/* Rest API
+Create  - post
+Read.   - get
+Update  - put
+Delete. - delete
+*/
+
+
+async RESTpost(   // proxyClass - client - side
+   buffer // create binary resource on server
+  ,url = window.location.href // server, default to current server
+) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/octet-stream'  //application/octet-stream
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: json // body data type must match "Content-Type" header
+  });
+  return response
+
+//----------------------
 
 async postJSON(   // proxyClass - client - side
    json // json messege to send
