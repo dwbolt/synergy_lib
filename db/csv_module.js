c1e51c26bf6a59e7c1d1d105e82604da776628b2
David Bolt <David.Bolt@Sfcknox.org>
2024-07-28 18:41:22 -0400

    test/fix csv_module import lgl data

--- db/csv_module.js
+++ db/csv_module.js
@@ -24,6 +24,11 @@ constructor(  // csvClass: client-side
   this.column     = 0    ;   // first column is 1
   this.nextN      = -1;
   this.line_count = {};
+
+  this.end_quoteR = undefined;  
+  this.end_quoteN = undefined;
+  this.end_quoteC = undefined;
+  this.end_array  = ["",]
 }
 
 
@@ -53,8 +58,8 @@ async parse_for_one_second(){   // csvClass: client-side
   this.display = new Date();  // time the parse process started
   //while ( this.valueStart <this.csv.length && (new Date() - this.display) < 1000  ) {  //if there is more to parse and a second has not passed, continue 
   let row   = this.row.toString();
-  while ( this.valueStart <this.csv.length ) {  //if there is more to parse and a second has not passed, continue 
-    if (this.parse_value()) {
+  while ( -1 < this.valueStart && this.valueStart < this.csv.length ) {  //if there is more to parse and a second has not passed, continue 
+    if (this.parse_value()) {   // true -> we have value false->end of line
       // we got a value, so add it the record
       this.column++;
       let col   = this.column.toString();
@@ -62,6 +67,7 @@ async parse_for_one_second(){   // csvClass: client-side
         this.select.push(col); // show the field
         this.fields[col] = {"header":col, "type": "string", "location":"column"}  // some maybe numbers, bool or other.
       }
+
       if (this.value !== undefined) {
         /* only doing strings at this point
         let t=typeof(this.value);
@@ -73,9 +79,10 @@ async parse_for_one_second(){   // csvClass: client-side
     } else {
       // we started a new line
       if (this.line_count[this.column] === undefined){
+        // no lines with this number of columns exist, so init to empty array
         this.line_count[this.column] = [];
       }
-      this.line_count[this.column].push(this.row);
+      this.line_count[this.column].push(this.row); // keep track of number of fields parsed from each row
 
       this.column = 0;   // start new column
       this.row++;        // start new row
@@ -86,18 +93,19 @@ async parse_for_one_second(){   // csvClass: client-side
     }
   }
  
-  if (this.more()) {
-      // show status
-    let row_counts = "";
-    const counts = Object.keys(this.line_count);
-    for(let i = 0; i<counts.length; i++) {
-      row_counts += `number of fields = ${counts[i]} - number of lines ${this.line_count[counts[i]].length}\n`
-    }
-    this.DOM.innerHTML = `${this.row} rows parsed total
+  // show status
+  let row_counts = "";
+  const counts = Object.keys(this.line_count);
+  for(let i = 0; i<counts.length; i++) {
+    row_counts += `number of fields = ${counts[i]} - number of lines ${this.line_count[counts[i]].length}\n`
+  }
+  this.DOM.innerHTML = `${this.row} rows parsed total
 ${this.row - this.row_old} rows parsed this time slice 
 next parse starting at = ${this.valueStart} file length=${this.csv.length}
 ${row_counts}`;
 
+/*
+  if (this.valueStart < this.csv.length) {
     this.row_old = this.row;
     setTimeout( this.parse_for_one_second.bind(this), 1)
   } else {
@@ -105,42 +113,38 @@ ${row_counts}`;
     //const changes = this.table.getJSON().changes;
     //changes.import = true;  
   }
-}
-
-
-more(){  // csvClass: client-side
-  // more to parse
-  return this.valueStart <this.csv.length
+  */
 }
 
 
 parse_value() {  // csvClass: client-side, return false if end of line, return true if value is parsed 
-  const start1 = this.csv[this.valueStart-1];
+  //const start_1 = this.csv[this.valueStart-1];
   const start  = this.csv[this.valueStart];
   if (this.nextN < this.valueStart){
-    this.nextN = this.csv.indexOf("\n", this.valueStart);
+    // get the first /n after start
+    let nextN = this.csv.indexOf("\n", this.valueStart);
+    if(this.nextN < nextN ) {
+      this.nextN = nextN
+    } else {
+      // missing \n at end of file
+      this.nextN = this.csv.length;
+    }
   }
-  if        (start1 === this.delimiter && start === this.delimiter )  {  // ,,  -> null
-    this.value = undefined;
+
+  switch (start) {
+  case this.delimiter:    // ,,  -> null
+    this.value = undefined;  // we have a value
     this.valueStart++;
     return true;
-  } else if (start === this.quote ) {
-//    ," this is Knoxville,TN",     -> string " this is Knoxville,TN"
-//    ," this is an escaped quote"""  -> string ' this is an escaped quote"'
-    this.value = this.parseQuote();
+  case this.quote :    
+    //    ," this is Knoxville,TN",     -> string " this is Knoxville,TN"
+    //    ," this is an escaped quote"""  -> string ' this is an escaped quote"'
+    this.value = this.parseQuote();  // we have a value
     return true;
-  } else if (start === "\n"  ) {
+  case "\n" :    
     this.valueStart++;
     return false;   // end of line
-  } else if (start === "\r" && start1 === "\n"  ) {
-    this.valueStart += 2;
-    return false;  // end of line
-  } else {
-    //  ,1,  -> number 1
-    //  , hello ,  -> string " hello "
-    if (start === this.delimiter) {
-      this.valueStart++;
-    }
+  default:
     this.value = this.parse();
     return true;
   }
@@ -148,85 +152,75 @@ parse_value() {  // csvClass: client-side, return false if end of line, return t
 
 
 parseQuote() {  // csvClass: client-side
-  let end = -1;
+  this.valueStart++;  // go past the starting quote
+
   // most common case
-  const end_comma    = this.csv.indexOf('",' , this.valueStart+1); 
-  const csv_part     = this.csv.slice(this.valueStart+1,end_comma);   
+  if ( (this.end_quoteR === undefined) || ( (0 < this.end_quoteR) && (this.end_quoteR < this.valueStart))) {this.end_quoteR = this.csv.indexOf('"\r' , this.valueStart);}
+  if ( (this.end_quoteN === undefined) || ( (0 < this.end_quoteN) && (this.end_quoteN < this.valueStart))) {this.end_quoteN = this.csv.indexOf('"\n' , this.valueStart);}
+  if ( (this.end_quoteC === undefined) || ( (0 < this.end_quoteC) && (this.end_quoteC < this.valueStart))) {this.end_quoteC = this.csv.indexOf('",'  , this.valueStart);}
+ 
+  let end = this.csv.length;
+  let inc   = 1;
+  if (0<this.end_quoteR) {end = Math.min(end,this.end_quoteR)}
+  if (0<this.end_quoteN) {end = Math.min(end,this.end_quoteN)}
+  if (0<this.end_quoteC) {end = Math.min(end,this.end_quoteC)}
+
+  let value = this.csv.slice(this.valueStart,end);
+  if (valueStart === end) {
+    // ,"",
+    value = undefined;
+    inc=2;
+  }
+  if (this.csv[end+inc] === '\r') {inc++;}  // start next parst on \n
+  this.valueStart = end + inc ;
+  return value;
+
+  //const csv_part     = this.csv.slice(this.valueStart+1,end_comma);   
 
   // see if end of line happend before end_comma
-  const end_of_line  = csv_part.indexOf('"\n'  ); 
-  const end_of_lineR = csv_part.indexOf('"\r\n');
-  let end_inc;
+  //const end_of_line  = csv_part.indexOf('"\n'  ); 
+  //const end_of_lineR = csv_part.indexOf('"\r\n');
+ 
+ /* let end_inc;
 
-  if (end_of_line === -1 && end_of_lineR === -1) {
+  if ( 0 < this.end_quoteC < this.nextN) {
     // end ",    the most common case
     end = end_comma;
     end_inc = 2;
-  } else if (0<end_of_line && end_ofLineR === -1) {
+  } else if (0<end_of_line && end_of_LineR === -1) {
     // end "/n    end of line next most common case
     end = this.valueStart + 1 +end_of_line;
     end_inc = 2;
-  } else if (-1===end_of_line && 0<end_ofLineR) {
+  } else if (-1===end_of_line && 0<end_of_LineR) {
     // end "/r/n   very rare
-    end = this.valueStart + 1 + end_ofLineR
+    end = this.valueStart + 1 + end_of_LineR
     end_inc = 3;
   } else {
     alert("error - parseQuote")
   }
-/*
-  if (0 < end_comma && end_comma < this.nextN) {
-    end = end_comma   // case  "            ",
-  } 
-  if (end<0) {
-    end_of_line  = this.csv.indexOf('"\n' , this.valueStart+1);  
-    if (0<end_of_line ){
-      end = end_of_line  // find the end of the quote   "            "\n
-    }
-  }
-*/
 
-//end = min(end_comma, end_of_line, )
-/*if (0 < end_comma && end_comma < this.nextN) {
-  end = end_comma   // case  "            ",
-} 
-if (end<0) {
-  end_of_line  = this.csv.indexOf('"\n' , this.valueStart+1);  
-  if (0<end_of_line ){
-    end = end_of_line  // find the end of the quote   "            "\n
-  }
-}
-*/
   let start       = this.valueStart+1;
   this.valueStart = end + end_inc;
   
-  return this.csv.slice(start, end);
-  /*
-  if        (0<end) {
-    let v = this.csv.slice( this.valueStart+1, end);
-    if (end_of_line === false) {
-      this.valueStart = end + 2 // get on the other side of "
-    } else {
-      this.valueStart = end + 1 // let start be \n so that next line code will trigger
-    }
-    
-    this.test_reverse(start, "parseQuote")
-    return(v); // return string value in array
-  } else {
-    alert(`errow file="csv_module.js" method="parseQuote" end=${end}`);
-    return null;
-  }*/
-
+  return this.csv.slice(start, end);*/
 }
 
 
 parse(){  // csvClass: client-side
   // find the end of the value, may come at next delimiter or end of line  \r\n or \n
-  let ed  = this.csv.indexOf(this.delimiter, this.valueStart);  // search for next delimiter
-  let end,v;
-  let start = this.valueStart;
+  let end_delimiter  = this.csv.indexOf(this.delimiter, this.valueStart);  // search for next delimiter
+  let value;
 
-  if (ed<0 && this.nextN<0) {
-    // end of line is missing
+  if ( (0<end_delimiter) &&  (end_delimiter < this.nextN) ) {
+    // most common case - not end of line
+    value = this.csv.slice( this.valueStart, end_delimiter);
+    this.valueStart = end_delimiter+1;
+  }  else {
+    // end of line
+    value = this.csv.slice( this.valueStart, this.end_value());
+    this.valueStart = this.nextN; // start on \n to trigger and end of line
+  }
+  /*
     v = this.csv.slice(this.valueStart);
     this.valueStart = this.csv.length; // will stop parse process
   } else if (ed<0 || (0<this.nextN && this.nextN<ed) ) {
@@ -242,9 +236,17 @@ parse(){  // csvClass: client-side
     v = this.csv.slice( this.valueStart, end )
     this.valueStart = end + 1;
   }
+*/
+ // this.test_reverse(start,"parse"); 
+  return value;
+}
 
-  this.test_reverse(start,"parse"); 
-  return v;
+end_value(){
+  if (this.csv[this.nextN-1] == "\r") {
+    return this.nextN-1;  // stop parse at \r
+  } else {
+    return this.nextN;    // stop at \n
+  }
 }
 
 
